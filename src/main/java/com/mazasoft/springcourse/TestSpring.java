9b9c7d652a79c6ef2785341a504b710250bea95b
vima1013 <viktor.mazepa@NetCracker.com>
2023-03-05 15:27:37 +0200

    Add example of Dependency Injection

--- src/main/java/com/mazasoft/springcourse/TestSpring.java
+++ src/main/java/com/mazasoft/springcourse/TestSpring.java
@@ -10,15 +10,19 @@ public class TestSpring {
     private static String APPLICATION_CONTEXT_FILE_NAME = "applicationContext.xml";
     private static String CLASSICAL_MUSIC_BEAN_NAME = "classicalMusicBean";
     private static String ROCK_MUSIC_BEAN_NAME = "rockMusicBean";
+    private static String MUSIC_PLAYER_BEAN_NAME = "musicPlayer";
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_FILE_NAME);
-        Music music = context.getBean(CLASSICAL_MUSIC_BEAN_NAME, Music.class);
+        /*   Music music = context.getBean(CLASSICAL_MUSIC_BEAN_NAME, Music.class);
         MusicPlayer musicPlayer = new MusicPlayer(music);
         musicPlayer.playMusic();
 
         music = context.getBean(ROCK_MUSIC_BEAN_NAME, Music.class);
         musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();*/
+
+        MusicPlayer musicPlayer = context.getBean(MUSIC_PLAYER_BEAN_NAME, MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
